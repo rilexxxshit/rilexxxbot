ad872cdd7e256ef51fbe73abdeefe3d7fb72293f
rilexxxshit <riansilva7674@gmail.com>
2021-05-14 08:11:12 -0300

    First commit

---
+++ src/daftarvip.js
@@ -0,0 +1,20 @@
+const daftarvip = (prefix) => { 
+	return `
+	
+*PREÇO DE LISTA VIP :*
+
+-Rp. 5 > Acessar recursos ViP
+-Rp. 8 > Recursos VIP + Insira o bot no seu grupo!
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/557988312466 ou digite *${prefix}dono*_
+
+*NOTA*
+
+*GRUPO DO RILEXXX 🐊🚩 :*
+https://chat.whatsapp.com/H5R3Lh8BJZFHbCatzzJMGH`
+}
+exports.daftarvip = daftarvip
---
+++ src/iklan.js
@@ -0,0 +1,18 @@
+const iklan = () => { 
+	return `           
+╔══✪〘 IKLAN 〙✪══
+║
+╠═══════════════════════════
+╠➥ *LISTA DE ALUGUEL E CRIAR BOTS:*
+╠➥ *ALUGUEL: 5 / GRUPO (MÊS)*
+╠➥ *BASE DO BOT: 15 (PODE SER PROPRIETÁRIO)*
+╠➥ *PODE PAGAR ATRAVÉS DE:*
+╠➥ BOLETO*
+╠═══════════════════════════
+╠➥ *VANTAGENS*
+╠➥ *wa.me/557988312466*
+║
+╚═〘  RILEXXX  〙
+`
+}
+exports.iklan = iklan
